009bb6c9925c3e98c1bb85c2c77f32b2dbe65f0e
Franklin Tello <franklintello@msn.com>
2022-11-25 19:02:28 -0700

    fixing practice code and comments

--- app_practice.py
+++ app_practice.py
@@ -7,11 +7,19 @@ from flask import Flask
 #create an app and assing variable name to app
 app = Flask(__name__)
 #define our starting point by setting our root using '/'
-#
+#This denotes that we want to put our data at the root of our routes. 
 @app.route('/')
-
+#create hello world function within our @app.route('/')
 def hello_world():
     return 'Hello world'
 
-#
+#run the app in your anaconda envioroment and navigate to your folder
+#where app is located type the following code: windows only
+#set FLASK_APP=app_practice.py
+#mac only: export export FLASK_APP=app.py
+
+#run the application 
+#Note: this is done still in your conda envioroment
+#code: flask run
+#copy and paste the http adress to web browser to see result
 
