cfabd3c797784df4189b67eb71f500dfa99b074c
Franklin Tello <franklintello@msn.com>
2022-11-25 18:44:52 -0700

    fixing code and comments

--- app_practice.py
+++ app_practice.py
@@ -1,37 +1,17 @@
-# Note: Using Git Bash to run website and Anaconda to run jupyter notebook
-# Could not get this to work with VSCode terminal.
+#inside of VSCode in order to run flask
+#hit ctrl+shift+p it will take you to searh type python: select interpreter 
+# select your conda envioroment pythondata base
 
-# open command prompt to update flask (both git bash and anaconda, not sure if it mattered)
-# pip install flask
-
-# inside of VSCode
-# update workspace (left side) to new folder surfs up, removed older workspaces
-# ctrl+shift+p python: select interpreter 
-# select surfs up folder, check using PythonData env
-# write code
+#import flask 
 from flask import Flask
+#create an app and assing variable name to app
 app = Flask(__name__)
+#define our starting point by setting our root using '/'
+#
 @app.route('/')
 
 def hello_world():
     return 'Hello world'
 
-#Skill Drill9.4.3:
-def star_trek():
-    return 'Welcome to the Collective!'
-
-# Back to the command prompt(git bash), run flask to get host website
-# export FLASK_APP=app.py
-# set FLASK_APP=app.py
-# flask run
-
-# get host website from command prompt
-# then paste into Google Chrome to see Flask run
-
-# Skill Drill 9.4.3
-# can use ctrl+C in the command prompt to quit running flask
-# made changes to VScode 
-# back to command prompt
-# only need to use flask run again to see the updated changes on the webiste
-# might need to refresh website to see the effect of the changed VSCode
+#
 
