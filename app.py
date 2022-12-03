c734dc705120f7c36c6ac23feebe6ab2bb771db0
Franklin Tello <franklintello@msn.com>
2022-12-02 17:24:42 -0700

    updating code and comments

--- app.py
+++ app.py
@@ -89,6 +89,64 @@ def precipitation():
    precip = {date: prcp for date, prcp in precipitation}
    #return the results of the query
    return jsonify(precip)
-#if you want to run the flask run it in cmd prompt and make sure when 
-#putting it into the browser to add the rout in this case http://127.0.0.1:5000/api/v1.0/precipitation
+# if you want to view this code run the flask in cmd prompt and add the route 
+#http://127.0.0.1:5000/api/v1.0/precipitation like so after you put it into browser
 
+# 9.5.4 Stations Route
+#As a reminder, this code should occur outside of 
+#the previous route and have no indentation. 
+#add a new route statations
+@app.route("/api/v1.0/stations")
+#defined the route function called stations
+def stations():
+    #create a variable results that will get all the stations
+    results = session.query(Station.station).all()
+    #start unraveling our results inot one dimensional arrays by using np.ravel ()
+    #with results as our parameters and converting it into a list by
+    #using list()
+    stations = list(np.ravel(results))
+    #conver the list into a json file by using jsnoify
+    return jsonify(stations=stations)
+# check website changes, (http://localhost:5000/), stations with USC0051xxxx codes
+
+#9.5.5 Monthly Temperature Route
+@app.route("/api/v1.0/tobs")
+
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+#do flask run, (http://localhost:5000/), block of temps (F)
+
+#9.5.6 Statistic Route
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+# flask run
+# After running this code, you'll be able to copy and paste the web address provided by Flask into a web browser. 
+# Open /api/v1.0/temp/start/end route and check to make sure you get the correct result, which is
+# [null,null,null]
+# You would add the following path to the address in your web browser:
+# /api/v1.0/temp/2017-06-01/2017-06-30
+# should return 
+# ["temps":[71.0,77.21989528795811,83.0]]
+
+# from tech help
+# if __name__ == '__main__':
+#     app.run()
